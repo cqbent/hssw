105d59bef27487c2d2a783614bc5476b570d4e5c
cqbent <cqbent@gmail.com>
2021-02-11 21:01:46 -0500

    misc style updates for colors, fonts, headers, etc.

--- wp-content/themes/hssw-sage/app/setup.php
+++ wp-content/themes/hssw-sage/app/setup.php
@@ -14,7 +14,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
     wp_enqueue_style('googleFonts', 'https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
-    wp_enqueue_style('googleFontsSerif', 'https://fonts.googleapis.com/css2?family=Esteban&display=swap');
+    wp_enqueue_style('googleFontsSerif', 'https://fonts.googleapis.com/css2?family=IM+Fell+Great+Primer&display=swap');
     wp_enqueue_style('fontAwesome', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css');
     if (is_single() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
